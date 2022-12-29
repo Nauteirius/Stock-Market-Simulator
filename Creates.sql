f694ca76b399252d3a8681cc2379775cc58e9148
Nauteirius <97364999+Nauteirius@users.noreply.github.com>
2022-12-29 07:22:15 +0100

    Add files via upload
    init

--- Creates.sql
+++ Creates.sql
@@ -1,6 +1,18 @@
 CREATE TABLE [Stock History] (
 [CompanyID] int primary key,
-Company NVARCHAR,
 [Stock Value] MONEY,
 [Date] DATE
 )
+
+CREATE TABLE [Companies] (
+[CompanyID] int primary key,
+[Company Name] NVARCHAR,
+Country NVARCHAR,
+CategoryID int
+)
+
+CREATE TABLE [Category] (
+CategoryID int primary key,
+[Category Name] NVARCHAR
+)
+
