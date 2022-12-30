9c5209ed34e11094d5dd1b6f38e89099c38549d6
Nauteirius <97364999+Nauteirius@users.noreply.github.com>
2022-12-30 18:04:29 +0100

    Add files via upload
    Preparation for uploading data from scv #2

--- Creates.sql
+++ Creates.sql
@@ -1,11 +1,33 @@
-CREATE TABLE [Stock History] (
-[CompanyID] int primary key,
-[Stock Value] MONEY,
-[Date] DATE
+--CREATE TABLE [Stock History] (
+--[CompanyID] int primary key,
+--[Stock Value] MONEY,
+--[Date] DATE
+--)
+----------------to bedzie w inicie
+
+CREATE table Transactions(
+TranslactionID int primary key,
+CompanyID int,
+UserID int,
+[Date] Date,
+Volumes int,
+
 )
 
+CREATE table Buy(
+[BuyingPrice] MONEY,
+TranslactionID int primary key,
+
+)
+
+CREATE table Sell(
+[SellingPrice] MONEY,
+TranslactionID int primary key,
+)
+-------------------------------------------------do dziedziczenia
+
 CREATE TABLE [Companies] (
-[CompanyID] int primary key,
+[CompanyID] nvarchar(5) primary key,
 [Company Name] NVARCHAR,
 Country NVARCHAR,
 CategoryID int
@@ -15,4 +37,3 @@ CREATE TABLE [Category] (
 CategoryID int primary key,
 [Category Name] NVARCHAR
 )
-
--- Users.sql
+++ Users.sql
@@ -1,10 +1,14 @@
-CREATE TABLE USERS (
-[UserID] int primary key,
-Pesel int,
-Password nvarchar(30),
+CREATE TABLE Users (
+[UserID] int primary key, --PESEL
 Sex nvarchar(1),
 Age int,
 )
 
 --if cyfra%2=1 -> 0 else 1 Sex check
 -- z peselu wyciagnac date urodzenia i sprawdzic ie pelnych lat od tego czasu oplynelo
+
+
+CREATE table Logins(
+[UserID] int primary key,
+Password nvarchar(30),
+)
